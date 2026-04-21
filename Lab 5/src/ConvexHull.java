2adc2fb7a5f64b80d478b8b80db2d168aeec03b4
Shivansh Garg <Shivanshhgargg@gmail.com>
2026-04-21 11:21:00 +0530

    Lab 8

--- Lab 5/src/ConvexHull.java
+++ Lab 5/src/ConvexHull.java
@@ -1,69 +1,90 @@
 import java.util.*;
 
+class Point {
+    double x, y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class ConvexHull {
-    public static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+
+    // Find orientation
+    static double orientation(Point a, Point b, Point c) {
+        return (b.x - a.x) * (c.y - a.y) -
+                (b.y - a.y) * (c.x - a.x);
     }
 
-    static int orientation(Point p, Point q, Point r) {
-        return (q.y - p.y) * (r.x - q.x) -
-                (q.x - p.x) * (r.y - q.y);
+    // Distance squared
+    static double distSq(Point a, Point b) {
+        return (a.x - b.x)*(a.x - b.x) +
+                (a.y - b.y)*(a.y - b.y);
     }
 
     public static List<Point> convexHull(List<Point> points) {
         int n = points.size();
-        if (n <= 1) return points;
+        if (n < 3) return points;
 
+        // Step 1: Find bottom-most point
+        Point p0 = points.get(0);
+        for (Point p : points) {
+            if (p.y < p0.y || (p.y == p0.y && p.x < p0.x)) {
+                p0 = p;
+            }
+        }
+
+        Point finalP0 = p0;
+
+        // Step 2: Sort by polar angle
         points.sort((a, b) -> {
-            if (a.x == b.x) return a.y - b.y;
-            return a.x - b.x;
+            double o = orientation(finalP0, a, b);
+            if (o == 0)
+                return Double.compare(distSq(finalP0, a), distSq(finalP0, b));
+            return (o > 0) ? -1 : 1;
         });
 
-        List<Point> lower = new ArrayList<>();
-        for (Point p : points) {
-            while (lower.size() >= 2 &&
-                    orientation(lower.get(lower.size()-2),
-                            lower.get(lower.size()-1), p) >= 0) {
-                lower.remove(lower.size()-1);
+        // Step 3: Stack for hull
+        Stack<Point> stack = new Stack<>();
+        stack.push(points.get(0));
+        stack.push(points.get(1));
+        stack.push(points.get(2));
+
+        for (int i = 3; i < n; i++) {
+            while (stack.size() > 1 &&
+                    orientation(nextToTop(stack), stack.peek(), points.get(i)) <= 0) {
+                stack.pop();
             }
-            lower.add(p);
+            stack.push(points.get(i));
         }
 
-        List<Point> upper = new ArrayList<>();
-        for (int i = n - 1; i >= 0; i--) {
-            Point p = points.get(i);
-            while (upper.size() >= 2 &&
-                    orientation(upper.get(upper.size()-2),
-                            upper.get(upper.size()-1), p) >= 0) {
-                upper.remove(upper.size()-1);
-            }
-            upper.add(p);
-        }
+        return new ArrayList<>(stack);
+    }
 
-        lower.remove(lower.size() - 1);
-        upper.remove(upper.size() - 1);
-
-        lower.addAll(upper);
-        return lower;
+    static Point nextToTop(Stack<Point> stack) {
+        Point top = stack.pop();
+        Point res = stack.peek();
+        stack.push(top);
+        return res;
     }
 
     public static void main(String[] args) {
         List<Point> points = Arrays.asList(
-                new Point(0,3), new Point(2,2),
-                new Point(1,1), new Point(2,1),
-                new Point(3,0), new Point(0,0),
-                new Point(3,3)
+                new Point(0, 3),
+                new Point(2, 2),
+                new Point(1, 1),
+                new Point(2, 1),
+                new Point(3, 0),
+                new Point(0, 0),
+                new Point(3, 3)
         );
 
         List<Point> hull = convexHull(points);
 
         System.out.println("Convex Hull:");
         for (Point p : hull) {
-            System.out.println(p.x + " " + p.y);
+            System.out.println("(" + p.x + ", " + p.y + ")");
         }
     }
 }
--- Lab 8/src/MatrixChainMultiplication.java
+++ Lab 8/src/MatrixChainMultiplication.java
@@ -1,3 +1,54 @@
+import java.util.*;
+
 public class MatrixChainMultiplication {
 
+    static int[][] m;
+    static int[][] s;
+
+    public static void matrixChainOrder(int[] p) {
+        int n = p.length - 1;
+        m = new int[n + 1][n + 1];
+        s = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            m[i][i] = 0;
+        }
+
+        for (int l = 2; l <= n; l++) { // chain length
+            for (int i = 1; i <= n - l + 1; i++) {
+                int j = i + l - 1;
+                m[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k < j; k++) {
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+
+                    if (q < m[i][j]) {
+                        m[i][j] = q;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void printOptimalParens(int i, int j) {
+        if (i == j) {
+            System.out.print("A" + i);
+        } else {
+            System.out.print("(");
+            printOptimalParens(i, s[i][j]);
+            printOptimalParens(s[i][j] + 1, j);
+            System.out.print(")");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] p = {30, 35, 15, 5, 10, 20, 25};
+
+        matrixChainOrder(p);
+
+        System.out.println("Number of multiplications: " + m[1][p.length - 1]);
+        System.out.print("Optimal Parenthesization: ");
+        printOptimalParens(1, p.length - 1);
+    }
 }
